021aa2ffd44690f9e32edeb0338f2e1d52331c83
GarethCooke <gareth_cooke@hotmail.com>
2024-03-03 18:27:52 +0000

    tidied up ui after angular update

--- beater/src/app/txt-message/txt-message.component.ts
+++ beater/src/app/txt-message/txt-message.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, SimpleChange } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatFormField } from '@angular/material/form-field';
